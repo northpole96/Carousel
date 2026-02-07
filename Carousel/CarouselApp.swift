c9aba181b557691a97594a9de4c3d20329905163
Rajesh <rajeshkhuntia00@gmail.com>
2026-02-07 21:41:59 +0530

    Initial Commit

---
+++ Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Rajesh Khuntia on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Carousel
+//
+//  Created by Rajesh Khuntia on 07/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
